1316bf3ec1db078f71ff2ac623183e137e5fafa9
Fernando Barajas <fernando.barajas@crowdint.com>
2015-03-27 11:38:04 -0600

    Bugfix: Gem can conflict with Devise/Warden
    - As mattbreeden pointed out on the issue #14, this gem has a
      conflict on Rails 4 with Devise/Warden, this commit uses
      the solution proposed by mattbreeden, in order to allow a user
      to login

--- lib/route_downcaser/railtie.rb
+++ lib/route_downcaser/railtie.rb
@@ -1,7 +1,8 @@
 module RouteDowncaser
   class Railtie < Rails::Railtie
     initializer "add_downcase_route_middleware" do |app|
-      app.config.middleware.use "RouteDowncaser::DowncaseRouteMiddleware"
+      app.config.middleware.insert_before 'Warden::Manager',
+        'RouteDowncaser::DowncaseRouteMiddleware'
     end
   end
 end
