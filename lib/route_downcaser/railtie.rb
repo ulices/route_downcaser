726d08150079996c514134dc09cf7ceb537bc8ca
Hector Bustillos <hector.bustillos@crowdint.com>
2015-04-01 10:15:53 -0600

    Remove middleware position, and redirect only get requests

--- lib/route_downcaser/railtie.rb
+++ lib/route_downcaser/railtie.rb
@@ -1,9 +1,6 @@
 module RouteDowncaser
   class Railtie < Rails::Railtie
-    initializer "add_downcase_route_middleware" do |app|
-      app.config.middleware.insert_before 'Warden::Manager',
-        'RouteDowncaser::DowncaseRouteMiddleware'
-    end
+    
   end
 end
 
