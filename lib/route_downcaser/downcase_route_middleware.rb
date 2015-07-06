7165761cac2d9706216b3ecbe12805fae7e7d313
Gilberto Villa <ingilniero@gmail.com>
2015-07-06 16:18:11 -0500

    Refactor call method
    * Stop checking for uri/path_info presence
    * Check for redirect config at the begining

--- lib/route_downcaser/downcase_route_middleware.rb
+++ lib/route_downcaser/downcase_route_middleware.rb
@@ -6,31 +6,21 @@ module RouteDowncaser
     end
 
     def call(env)
-      # Don't touch anything, if uri/path is part of exclude_patterns
-      if (!exclude_patterns_match?(env['REQUEST_URI']) || !exclude_patterns_match?(env['PATH_INFO'])) && env['REQUEST_METHOD'] == "GET"
-        # Downcase request_uri and/or path_info if applicable
-        if env['REQUEST_URI'].present?
-          request_uri = downcased_uri(env['REQUEST_URI'])
-        end
 
-        if env['PATH_INFO'].present?
-          request_path_info = downcased_uri(env['PATH_INFO'])
-        end
+      if RouteDowncaser.redirect
+        # Don't touch anything, if uri/path is part of exclude_patterns
+        if (!exclude_patterns_match?(env['REQUEST_URI']) ||
+            !exclude_patterns_match?(env['PATH_INFO'])) && env['REQUEST_METHOD'] == "GET"
 
-        # If redirect configured, then return redirect request,
-        # if either request_uri or path_info has changed
-        if RouteDowncaser.redirect
-          if env["REQUEST_URI"].present? and request_uri != env["REQUEST_URI"]
-            return redirect_header(request_uri)
+          url = downcased_uri(env['REQUEST_URI'] || env['PATH_INFO'])
+
+          #redirect request if either request_uri or path_info has changed
+          if url != (env['REQUEST_URI'] || env['PATH_INFO'])
+            return redirect_header(url)
           end
 
-          if env["PATH_INFO"].present? and request_path_info != env["PATH_INFO"]
-            return redirect_header(request_path_info)
-          end
         end
-
       end
-
       # Default just move to next chain in Rack callstack
       # calling with downcased uri if needed
       @app.call(env)
